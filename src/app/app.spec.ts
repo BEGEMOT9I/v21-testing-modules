8ba8020532d05de7fc6a2624728f9b51db57fa34
begemot9i <daniil.demenev@workato.com>
2026-02-02 22:24:49 +0100

    chore: added comment

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
-import type { AlarmType } from "../alarm/alarm.types";
+import type { AlarmType } from "../alarm/alarm.types"; // <- comment out to passs the case
 
 @Component({
   selector: 'test-component',
